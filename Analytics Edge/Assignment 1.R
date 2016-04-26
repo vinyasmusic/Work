84bfdea074932f06ba1f2b47c068a349c44f202e
vinyasmusic <vinyasmusic@gmail.com>
2016-04-26 06:52:17 +0530

    Week1
    Completion of Week 1 for both courses

--- Analytics Edge/Assignment 1.R
+++ Analytics Edge/Assignment 1.R
@@ -1,4 +1,5 @@
 #Analytical Detective
+
 mvt=read.csv("Data Files/mvtWeek1.csv") #Read file
 nrow(mvt) #Number of entries
 ncol(mvt) #number of variables
@@ -28,3 +29,47 @@ table(TOP5$LocationDescription,TOP5$Weekday)
 
 
 #Stock Dynamics
+
+#Read Files
+ibm=read.csv("Data Files/IBMStock.csv")
+ge=read.csv("Data Files/GEStock.csv")
+pg=read.csv("Data Files/ProcterGambleStock.csv")
+cola=read.csv("Data Files/CocaColaStock.csv")
+boeing=read.csv("Data Files/BoeingStock.csv")
+
+#Change the date format
+ibm$Date = as.Date(ibm$Date, "%m/%d/%y")
+cola$Date = as.Date(cola$Date, "%m/%d/%y")
+ge$Date = as.Date(ge$Date, "%m/%d/%y")
+pg$Date = as.Date(pg$Date, "%m/%d/%y")
+boeing$Date = as.Date(boeing$Date, "%m/%d/%y")
+
+tail(boeing$Date,1) #print the last date
+summary(ibm)
+mean(ibm)
+which.max(cola$StockPrice) #Maximum stock price of cola 
+sd(pg$StockPrice) #Standard deviation of the stock price for PG
+
+plot(cola$Date,cola$StockPrice,type = "l") #Plot Date on X and Stock price on Y axis wrt CocoCola, type l means line graph
+
+lines(pg$Date,pg$StockPrice,col="blue") #Add line graph for PG on the plot with colour blue
+abline(v=as.Date(c("2000-03-01")), lwd=2) #Draw a line ||el to Y on given date
+plot(cola$Date,cola$StockPrice,type = "l",col="red")
+lines(pg$Date,pg$StockPrice,col="blue")
+plot(cola$Date[301:432], cola$StockPrice[301:432], type="l", col="red", ylim=c(0,210))
+lines(pg$Date[301:432], pg$StockPrice[301:432], type="l", col="blue")
+lines(ge$Date[301:432], ge$StockPrice[301:432], type="l", col="green")
+lines(ibm$Date[301:432], ibm$StockPrice[301:432], type="l", col="orange")
+lines(boeing$Date[301:432], boeing$StockPrice[301:432], type="l", lty=3) #ty=3 gives a dotted line
+
+t1=tapply(ibm$StockPrice,months(ibm$Date),mean) #Table with mean stock price acc to month
+t2=tapply(ge$StockPrice,months(ge$Date),mean)
+t3=tapply(pg$StockPrice,months(pg$Date),mean)
+t4=tapply(boeing$StockPrice,months(boeing$Date),mean)
+t5=tapply(cola$StockPrice,months(cola$Date),mean)
+
+#Demographics
+
+CPS=read.csv("Data Files/CPSData.csv") 
+tapply( cps$Country!="United States",cps$MetroArea=="New York-Northern New Jersey-Long Island, NY-NJ-PA",mean,na.rm=TRUE)
+sort(tapply( cps$Country=="Somalia",cps$MetroArea,sum,na.rm=TRUE))
